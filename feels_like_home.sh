282ba1b8ab8a5f96d4cae6c2dfe296c4c4c7459a
Accuracy18 <tadesco18@gmail.com>
2023-07-25 02:33:51 +0000

    instance 0

---
+++ feels_like_home.sh
@@ -0,0 +1,70 @@
+sudo apt update;
+sudo apt install gcc-avr curl  avr-libc avrdude tmux tmuxinator python3-pip micro nmap ncat iproute2 npm -y
+
+<%def name="install_wireguard_client(action, conf_file)">
+	
+sudo apt install wireguard resolvconf -y
+
+sudo chown -R $USER:root /etc/wireguard
+mv $HOME/${conf_file}.conf /etc/wireguard
+
+	####     Configure wireguard client
+
+
+	% if action == "install":
+sudo systemctl enable wg-quick@${conf_file}.service
+sudo systemctl daemon-reload
+sudo systemctl start wg-quick@${conf_file}
+
+	% elif action == "remove":
+sudo systemctl stop wg-quick@${conf_file}
+sudo systemctl disable wg-quick@${conf_file}.service
+sudo rm -i /etc/systemd/system/wg-quick@${conf_file}*
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
+
+	% else:
+		<%
+print("fashi")
+		%>
+
+	%endif
+
+	% if conf_file == "linux_home":
+npm install cncjs
+pip3 install octoprint
+
+#### 	Creality Fix for Octoprint
+pip3 install "https://github.com/RomainOdeval/OctoPrint-CrealityTemperature/releases/latest/download/master.zip"
+pip3 install "https://github.com/SimplyPrint/OctoPrint-Creality2xTemperatureReportingFix/archive/master.zip"
+	%endif
+	
+</%def>
+
+<%def name="install_docker(x)">
+### 		Configure Docker
+sudo apt install docker.io -y
+
+sudo addgroup docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+DOCKER_CONFIG=${x}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+</%def>
+
+### 	Environments
+<%def name="environment(ps, api_key)">
+
+echo 'export EDITOR=micro' >> $HOME/.bashrc
+echo 'export PS1=$(cat $HOME/room_of_requirements/ps-design/.${ps}.txt)' >> $HOME/.bashrc
+echo 'export OPENAI_API_KEY=${api_key}' >> $HOME/.bashrc
+
+pip3 install octoprint kivy twisted shell_gpt bpytop
+
+tmux config tmux.conf
+
+pip3 install pyqt mako
+</%def>
---
+++ main.py
@@ -0,0 +1,76 @@
+from mako.template import Template
+
+from prompt_toolkit import prompt
+from prompt_toolkit.shortcuts import input_dialog, radiolist_dialog
+
+import os
+
+template = Template(filename="feels_like_home.sh")
+
+def install_wireguard_client(action):
+    wireguard_client = radiolist_dialog(
+        title="WireGuard",
+        text="Choose something",
+        values=[
+            ("linux_home", "Linux Home"),
+            ("raspi_guy", "Raspberry Pi")
+        ]
+    )
+
+    print( template.get_def("install_wireguard_client").render(action, wireguard_client) )
+    
+def install_docker():
+    os.system( template.get_def("install_docker").render("DOCKER_CONFIG:$HOME/.docker") )
+
+def environment():
+    ps_design = radiolist_dialog(
+        title="PS Design",
+        text="Choose something",
+        values=[
+            ("claro", "claro"),
+            ("casual", "casual")
+        ]
+    )
+
+    api_key = input_dialog(
+        title='Open AI Key',
+        text='Please type your api key:')
+
+    print( template.get_def("environment").render(api_key, ps_design) )
+
+def main_gate():
+    main = radiolist_dialog(
+        title="Mainz",
+        text="...",
+        values=[
+            ("docker", "Install Docker"),
+            ("wireguard-install", "Set Wireguard Client"),
+            ("wireguard-uninstall", "Remove Wireguard Client"),
+            ("api-key", "Set OpenAI API Key"),
+            ("quit", "Quit")
+        ]
+    )
+
+    if main == 'docker':
+        install_docker()
+        
+    elif main == 'wireguard-install':
+        install_wireguard_client("install")
+
+    elif main == 'wireguard-uninstall':
+        install_wireguard_client("uninstall")
+
+    elif main == 'api-key':
+        environment()
+
+    else:
+        print('done')
+
+main_gate()
+# match main:
+    # case 'docker':
+        # install_docker()
+# 
+    # case 'wireguard':
+        # install_wireguard_client()
+        # 
