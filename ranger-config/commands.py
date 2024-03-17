1f9dbd2aa017c109f37e0c1d5dec241a772e9548
Accuracy18 <eojo1@uncc.edu>
2024-03-17 16:20:47 +0000

    mar17

--- ranger-config/commands.py
+++ ranger-config/commands.py
@@ -9,4 +9,8 @@ class KubernetesApply(Command):
             try:
                 subprocess.run(f'kubectl apply -f {item}'.split(), check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             except subprocess.CalledProcessError as e:
-                print(e)
+                self.fm.notify(e)
+
+class OpenK9S(Command):
+    def execute(self):
+        subprocess.run(['k9s'], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
