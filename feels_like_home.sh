725b2a495795060d17a75e74775133b6413d1259
Accuracy18 <tadesco18@gmail.com>
2024-03-30 13:38:05 -0400

    mar30

--- feels_like_home.sh
+++ feels_like_home.sh
@@ -1,7 +1,7 @@
 
 <%def name="general()">
 sudo apt update
-sudo apt install gcc-avr curl ranger  avr-libc avrdude tmux tmuxinator wireguard resolvconf python3-pip micro nmap ncat iproute2 npm -y
+sudo apt install gcc-avr curl ranger  avr-libc avrdude tmux tmuxinator wireguard resolvconf python3-pip micro nmap ncat iproute2 npm docker.io -y
 
 npm install cncjs
 pip3 install octoprint kivy twisted shell_gpt bpytop pyqt mako
@@ -12,16 +12,17 @@ pip3 install "https://github.com/SimplyPrint/OctoPrint-Creality2xTemperatureRepo
 </%def>
 
 <%def name="install_docker(x)">
-### 		Configure Docker
-sudo apt install docker.io -y
 
 sudo addgroup docker
 sudo usermod -aG docker $USER
 newgrp docker
 
-DOCKER_CONFIG=${x}
 mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose</%def>
+
+curl -SL https://github.com/docker/compose/releases/download/v2.26.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+
+sudo chmod +x $HOME/.docker/cli-plugins/docker-compose
+</%def>
 
 ### 	Environments
 <%def name="environment(api_key, ps)">
