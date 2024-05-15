ef7324348643a0bf7e11ed609620470a81eefadf
Accuracy18 <tadesco18@gmail.com>
2024-05-15 13:43:43 -0400

    may15

--- micro-config/AIplugin/gemini.py
+++ micro-config/AIplugin/gemini.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python3
-import google.generativeai as genai, argparse, os
+import google.generativeai as genai, argparse, os # type: ignore
 
 parser = argparse.ArgumentParser(description='Your program description')
 
@@ -10,5 +10,6 @@ args = parser.parse_args()
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 model = genai.GenerativeModel('gemini-1.0-pro-latest')
+instruction = "Instructions: just give the code without text formatting, remove anything involving '''python''': "
 response = model.generate_content(args.request)
 print(response.text)
--- micro-config/AIplugin/main.lua
+++ micro-config/AIplugin/main.lua
@@ -1,17 +1,34 @@
 local micro = import('micro')
 local config = import('micro/config')
 local shell = import('micro/shell')
+local util = import('micro/util')
 
 function onBufferOpen(bp)
 	config.MakeCommand("gemini", Gemini, config.NoComplete)
 end
 
 function Gemini(bp, args)
-    local commandArgs = {}
-    
-	local request = shell.ExecCommand('cgemini', '-r', args[1])
-    bp.Buf:Insert(-bp.Cursor.Loc, request)
+    cursor = bp.Cursor
+            
+    selection = cursor:GetSelection()
 
+    selection = util.String(selection)
+
+    local prompt = ""
+    
+    if cursor:HasSelection() then
+        prompt = selection
+        micro.InfoBar():Message(5)
+
+    else
+        prompt = args[1]
+        micro.InfoBar():Message(6)
+    end
+    
+	local request = shell.ExecCommand('cgemini', '-r', prompt)
+
+    bp.Buf:Insert(-bp.Cursor.Loc, request)
+    
+    --micro.InfoBar():Message(selection)
     --bp:InsertNewline()
-    --micro.InfoBar():Message(args[1])
 end
