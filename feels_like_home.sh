302dbf6ff3fd0f615b9db386328637061fd60933
Accuracy18 <tadesco18@gmail.com>
2025-06-21 02:44:01 +0000

    jun20

--- feels_like_home.sh
+++ feels_like_home.sh
@@ -1,20 +1,33 @@
 sudo apt update
 sudo apt install gcc-avr curl ranger git python3-twisted avr-libc bpytop avrdude tmux tmuxinator resolvconf python3-pip micro nmap iproute2 npm -y
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-mv kubectl ~/.local/bin
+# Install Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin" >> $HOME/.bashrc
 
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-amd64
-chmod +x ./kind
-sudo mv ./kind ~/.local/bin
-
-curl -fsSL https://ollama.com/install.sh | sh
+# curl -fsSL https://ollama.com/install.sh | sh
 
 cp config/tmux.conf $HOME/.tmux.conf
 tmux source $HOME/.tmux.conf
 
 cp config/casual.sh $HOME/.casual.sh
 echo 'source $HOME/.casual.sh' >> $HOME/.bashrc
+
 source $HOME/.bashrc
 echo 'export EDITOR=micro' >> $HOME/.bashrc
-# source ai/bin/activate
+
+# Install Docker
+sudo snap install docker
+sudo addgroup docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo snap disable docker
+sudo snap enable docker
+
+# Prepare Git clones
+git config --global user.email "tadesco18@gmail.com"
+git config --global user.name "Accuracy18"
+git clone git@github.com:Accuracy18/Services.git
+
+# All TUIs
+brew install ducker bpytop
